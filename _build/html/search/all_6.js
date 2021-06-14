2b5abc81305f66a1d8a66b6fa2af63d981394b7c
AliceNardelli <73175752+AliceNardelli@users.noreply.github.com>
2021-06-14 19:35:09 +0200

    Delete _build directory

--- _build/html/search/all_6.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['fix_5ffinal_5fyaw_17',['fix_final_yaw',['../classgo__to__point_1_1_goal_reaching_action.html#a8590a161935bcbdacc3619c3e5326f03',1,'go_to_point::GoalReachingAction']]],
-  ['fix_5fyaw_18',['fix_yaw',['../classgo__to__point_1_1_goal_reaching_action.html#a421d818d46da3ac2f08fe0c745f055c5',1,'go_to_point::GoalReachingAction']]],
-  ['first_20assignment_20of_20research_20trach_202_20course_20_2d_20action_20branch_19',['First Assignment of Research Trach 2 course - action branch',['../md__r_e_a_d_m_e.html',1,'']]]
-];
--- _build/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['first_20assignment_20of_20research_20trach_202_20course_20_2d_20action_20branch_115',['First Assignment of Research Trach 2 course - action branch',['../md__r_e_a_d_m_e.html',1,'']]]
-];
